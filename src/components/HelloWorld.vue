bdfce0e43c8a5eb57f567cde0a269d6da01d23af
takanuki <genka8725@gmail.com>
2021-02-22 12:46:52 +0900

    edit:スロット追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,55 +1,74 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button @click="handleClick">カウント{{ count }}</button>
-    <!-- <button @click="handleClick2">減算{{ count }}</button> -->
-    <CopyDocs />
-    <RefLinks />
+    <button @click="handleClick">カウント {{ count }}</button>
+    <CopyDocs docs="For a guide and recipes on how to configure / customize this project," />
+    <RefLinks #default="{ links }">
+        <div v-for="link in links" :key="link.id">
+        <h3>{{ link.title }}</h3>
+        <ul>
+            <li v-for="li in link.list" :key="li.id">
+            <a :href="li.href" target="_blank" rel="noopener">
+                {{ li.text }}
+            </a>
+            </li>
+        </ul>
+        </div>
+    </RefLinks>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import RefLinks from "./RefLinks.vue";
-import CopyDocs from "./CopyDocs.vue";
+import { defineComponent } from 'vue';
+import RefLinks from './RefLinks.vue'
+import CopyDocs from './CopyDocs.vue'
 
 type Props = {
-  msg: string;
-  count: number;
-};
+  msg: string,
+  count: number
+}
+
 export default defineComponent({
-  name: "HelloWorld",
-  components: {
-    RefLinks,
-    CopyDocs,
-  },
+  name: 'HelloWorld',
+  components: { RefLinks, CopyDocs },
   props: {
     msg: {
       type: String,
-      required: true,
+      required: true
     },
     count: {
       type: Number,
-      required: true,
-    },
+      required: true
+    }
   },
-  //   emits: ["child-event"],
-  setup(props: Props, { emit }) {
+  emits: ['child-event'],
+  setup (props: Props, { emit }) {
     const handleClick = () => {
-      emit("child-event", props.count + 1);
-    };
-    // const handleClick2 = () => {
-    //   emit("child-event2", props.count - 1);
-    // };
-    return { handleClick };
-
-  },
+      emit('child-event', props.count + 1)
+    }
+    return { handleClick }
+  }
 });
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 button {
   padding: 15px;
   font-size: 1.8rem;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- src/components/RefLinks.vue
+++ src/components/RefLinks.vue
@@ -1,100 +1,58 @@
 <template>
   <div>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+      <slot :links="links"></slot>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
-  name: 'RefLinks',
+  name: "RefLinks",
+  setup() {
+    const links =
+      reactive({
+        'installed': {
+          title: "Installed CLI Plugins",
+          list: [
+            {
+              href: "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel",
+              text: "babel",
+            },
+            {
+              href: "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript",
+              text: "typescript",
+            }
+          ]
+        },
+        'essential': {
+          title: "Essential Links",
+          list: [
+            { href: "https://vuejs.org", text: "Core Docs" },
+            { href: "https://forum.vuejs.org", text: "Forum" },
+            { href: "https://chat.vuejs.org", text: "Community Chat" },
+            { href: "https://twitter.com/vuejs", text: "Twitter" },
+            { href: "https://news.vuejs.org", text: "News" }
+          ]
+        },
+        'ecosyste': {
+          title: "Ecosystem",
+          list: [
+            { href: "https://router.vuejs.org", text: "vue-router" },
+            { href: "https://vuex.vuejs.org", text: "vuex" },
+            {
+              href: "https://github.com/vuejs/vue-devtools#vue-devtools",
+              text: "vue-devtools",
+            },
+            { href: "https://vue-loader.vuejs.org", text: "vue-loader" },
+            {
+              href: "https://github.com/vuejs/awesome-vue",
+              text: "awesome-vue",
+            },
+          ]
+        }
+      })
+    return { links }
+  },
 });
 </script>
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-</style>
